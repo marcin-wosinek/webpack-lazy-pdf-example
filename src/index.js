6e8ae87bd3cdd69ad9b5dccdcf583888a68cfcc4
Marcin Wosinek <marcin.wosinek@gmail.com>
2021-04-19 01:09:20 +0100

    add pdfkit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// require dependencies
+const PDFDocument = require("pdfkit");
+
+// create a document the same way as above
+const doc = new PDFDocument();
+
 const element = document.getElementById("core");
 
 element.innerHTML = "Main app is loaded";
